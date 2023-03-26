6b9cdb03fe6eede4f3e76f1adfee50b01440e1ea
GustavojSaraiva <102226082+GustavojSaraiva@users.noreply.github.com>
2023-03-26 20:33:53 -0300

    Add files via upload

---
+++ pdf.php
@@ -0,0 +1,61 @@
+<?php
+require('FPDF185/FPDF.php');
+
+class PDF extends FPDF
+{
+// Load data
+function LoadData($file)
+{
+    // Read file lines
+    $lines = file($file);
+    $data = array();
+    foreach($lines as $line)
+        $data[] = explode(';',trim($line));
+    return $data;
+}
+
+
+// Colored table
+function FancyTable($header, $data)
+{
+    // Colors, line width and bold font
+    $this->SetFillColor(255,0,0);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(128,0,0);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // Header
+    $w = array(30, 30, 60, 30);
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+    $this->Ln();
+    // Color and font restoration
+    $this->SetFillColor(224,235,255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Data
+    $fill = false;
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+        $this->Cell($w[0],6,$row[1],'LR',0,'L',$fill);
+        $this->Cell($w[2],6,$row[2],'LR',0,'L',$fill);
+        $this->Cell($w[0],6,$row[3],'LR',0,'L',$fill);
+        $this->Ln();
+        $fill = !$fill;
+    }
+    // Closing line
+    $this->Cell(array_sum($w),0,'','T');
+}
+}
+
+$pdf = new PDF();
+// Column headings
+$header = array('Nome', 'Curso', 'Diciplina', 'Media');
+// Data loading
+$data = $pdf->LoadData('alunos.csv');
+$pdf->SetFont('Arial','',8);
+$pdf->AddPage();
+$pdf->FancyTable($header,$data);
+$pdf->Output();
+?>
